990b6ec3efe15672fa72f69793b741171306d59b
austin_smith <trunk9slamchest@gmail.com>
2019-10-11 18:08:54 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,22 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def new
-    @post = Post.new
-  end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  # add create method here
+	def new
+		@post = Post.new
+	end
+
+	def create
+	# byebug
+		Post.create(title: params[:title], description: params[:description])
+
+		redirect_to '/posts'
+	end
+
 end
